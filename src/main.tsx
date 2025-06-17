477a953f93e5da56c6b984e5b05368df4e6193dc
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-06-17 16:24:51 -0500

    feat: initial project structure

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import "@styles/App.css";
+import "@styles/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+    </StrictMode>,
+);
