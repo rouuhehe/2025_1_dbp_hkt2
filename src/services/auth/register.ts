b6ee978613c0ee79b9975707a565eb4cfec01a51
Ela <miruxchiru@gmail.com>
2025-06-17 17:24:19 -0500

    registeryaaaa

--- src/services/auth/register.ts
+++ src/services/auth/register.ts
@@ -1,13 +1,21 @@
 import Api from "@services/api";
 import { RegisterRequest } from "src/interface/auth/RegisterRequest";
+import axios from "axios";
 
 export async function register(registerRequest: RegisterRequest) {
     try {
         const api = await Api.getInstance();
         await api.post<RegisterRequest, void>(
-            registerRequest, { url: "/auth/register" }
+            registerRequest, { url: "/authentication/register" }
         )
-    } catch (error) {
-        throw new Error("Registro fallido")
-    }
+    } catch (error: any) {
+			if (axios.isAxiosError(error)) {
+				if (error.response?.status === 400) {
+					throw new Error("El correo ya está registrado.");
+				} else {
+					throw new Error("Error del servidor. Intenta más tarde.");
+				}
+			}
+			throw new Error("Error desconocido.");
+		}
 }
